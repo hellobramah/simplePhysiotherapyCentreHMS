538080b69957f0280a113c9a13e543bd730018a4
Alex Bramah-Lawani <hellobramah@gmail.com>
2023-09-18 19:20:46 +0000

    Use env variables and create Procfile.

--- simple_HMS/__init__.py
+++ simple_HMS/__init__.py
@@ -5,16 +5,22 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
 from flask_gravatar import Gravatar
+import os
 from os import path
 
 database = SQLAlchemy()
+
+
 DB_NAME = "posts.db"
+
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    database.init_app(app)
+    app.config['SECRET_KEY'] = os.environ.get("FLASK_SECRET_KEY")
+    #app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_LOCATION", "sqlite:///posts.db")
+
+    #database.init_app(app)
     # Configure CKEditor
     ckeditor = CKEditor(app)
     # Configure Bootstrap5
@@ -33,12 +39,11 @@ def create_app():
                         use_ssl=False,
                         base_url=None)
 
-
-
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
     from .models import User, BlogPost, Comment
+    database.init_app(app)
     create_database(app)
 
     login_manager = LoginManager()
@@ -49,17 +54,19 @@ def create_app():
     def load_user(id):
         return User.query.get(int(id))
 
-
     return app
 
 
-
 def create_database(app):
-    db_path = path.join(app.instance_path, DB_NAME)
-    print(f'Database Path !{db_path}')
-    if not path.exists(db_path):
-        with app.app_context():
-            database.create_all()
-        print('Created Database!')
-    else:
-        print('Database already exists.')
+    with app.app_context():
+        database.create_all()
+    return 'Database tables created successfully'
+
+    # db_path = path.join(app.instance_path, DB_NAME)
+    # # print(f'Database Path !{db_path}')
+    # if not path.exists(db_path):
+    #     with app.app_context():
+    #         database.create_all()
+    #     print('Created Database!')
+    # else:
+    #     print('Database already exists.')
