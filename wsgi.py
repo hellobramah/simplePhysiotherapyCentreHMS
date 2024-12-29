7aa736eb9142ea58b921c267a10e4d94f5e39f86
Alex Bramah-Lawani <hellobramah@gmail.com>
2024-12-29 20:29:26 +0000

    Add .cpanel.yml for deployment

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,8 @@
 from dotenv import load_dotenv
-
 load_dotenv()
+
 import os
 import sys
 
-project_path = os.getenv('PROJECT_PATH', '/home/default_placeholder_path/public_html/seaside-hms')
+project_path = os.getenv('PROJECT_PATH', '/default/path')
 sys.path.insert(0, project_path)
-
-from main import app as application
