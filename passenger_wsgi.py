09cd20e1125c810b42d44d7aac79b8cbd051f1e2
Alex Bramah-Lawani <hellobramah@gmail.com>
2024-12-30 07:45:07 +0000

    update passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-# Dynamically resolve the application path
+
 app_path = os.getenv('APP_PATH')
 if not app_path:
     raise RuntimeError("Environment variable APP_PATH is not set.")
@@ -9,4 +9,4 @@ if not app_path:
 sys.path.insert(0, app_path)
 
 # Load the application
-from main import app as application  # Ensure `main.py` contains your Flask app instance
+from main import app as application  
