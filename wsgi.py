f103f7952ca1713118382a6514cb92acce47c9b3
Alex Bramah-Lawani <hellobramah@gmail.com>
2024-12-30 04:28:26 +0000

    Add passenger_wsgi.py for cPanel deployment

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,13 @@
-from dotenv import load_dotenv
-load_dotenv()
-
 import os
 import sys
 
-project_path = os.getenv('PROJECT_PATH', '/default/path')
-sys.path.insert(0, project_path)
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+
+def application(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    message = 'It works!\n'
+    version = 'Python v' + sys.version.split()[0] + '\n'
+    response = '\n'.join([message, version])
+    return [response.encode()]
