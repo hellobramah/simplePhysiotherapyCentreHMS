66230920b122fceafd398beeff700949c59e7977
Alex Bramah-Lawani <hellobramah@gmail.com>
2024-12-30 07:58:29 +0000

    Save local changes to passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,3 +1,4 @@
+import imp
 import os
 import sys
 
@@ -6,7 +7,10 @@ app_path = os.getenv('APP_PATH')
 if not app_path:
     raise RuntimeError("Environment variable APP_PATH is not set.")
 
+
+
 sys.path.insert(0, app_path)
 
-# Load the application
+
+
 from main import app as application  
