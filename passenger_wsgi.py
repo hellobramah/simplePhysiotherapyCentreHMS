21a9cc1470436e34faeb5bc99c5e6e878f4e5d2a
Alex Bramah-Lawani <hellobramah@gmail.com>
2024-12-31 16:02:55 +0000

    modify passenger_wsgi.py to include environment variables

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,12 +1,17 @@
 import os
 import sys
 
-# Adjust to your symlink path
-app_path = os.getenv('APP_ROOT', '/home/hwzfvzql/repositories/simplePhysiotherapyCentreHMS')
+base_dir = os.getenv('APP_BASE_DIR', '/default/path/to/app')
+if not base_dir:
+    raise RuntimeError("Environment variable APP_BASE_DIR is not set.")
 
-if not app_path:
-    raise RuntimeError("APP_ROOT environment variable not set.")
+sys.path.insert(0, base_dir)
 
-sys.path.insert(0, app_path)
+activate_env = os.path.join(base_dir, os.getenv('VENV_RELATIVE_PATH', '../3.9/bin/activate_this.py'))
+if os.path.exists(activate_env):
+    with open(activate_env) as file_:
+        exec(file_.read(), dict(__file__=activate_env))
+else:
+    raise RuntimeError(f"Virtual environment activation script not found at {activate_env}")
 
 from main import app as application
